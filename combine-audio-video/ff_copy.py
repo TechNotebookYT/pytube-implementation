84f9e6c01cbf3d0e39b1e61cb04771d1eaa4e612
chromebook12 <90718165+chromebook12@users.noreply.github.com>
2021-10-15 12:35:56 -0500

    added comments for additonal clarity

--- combine-audio-video/ff_copy.py
+++ combine-audio-video/ff_copy.py
@@ -1,4 +1,13 @@
-def copy(video_path, audio_path, final_path, name):
+def copy(video_path, audio_path, name, final_path):
+    """
+    FFMPEG-COPY
+    ------
+
+    video_path: path of video on disk
+    audio_path: path of audio on disk
+    name: name of output file
+    final_path: path of output video on disk
+    """
     import ffmpeg
     video = ffmpeg.input(audio_path)
     audio = ffmpeg.input(video_path)
