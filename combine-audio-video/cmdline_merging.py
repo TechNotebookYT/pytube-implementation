aab76586c1457f404e8b7795264495687bd9cb7b
Pranav Bala <technotebook@yahoo.com>
2021-11-22 21:44:38 -0600

    rewrote & commented file

--- combine-audio-video/cmdline_merging.py
+++ combine-audio-video/cmdline_merging.py
@@ -1,10 +1,13 @@
+# Imports
 from pytube import YouTube
 import os
 import shutil
+import ffmpeg
 
-def copy(video_path, audio_path, name, final_path):
+
+def merge(video_path, audio_path, name, final_path):
     """
-    FFMPEG-COPY
+    FFMPEG-Merge
     ------
 
     video_path: path of video on disk
@@ -12,41 +15,44 @@ def copy(video_path, audio_path, name, final_path):
     name: name of output file
     final_path: path of output video on disk
     """
-    import ffmpeg
-    video = ffmpeg.input(audio_path)
-    audio = ffmpeg.input(video_path)
-    (
-        ffmpeg
-        .output(audio, video, f'{final_path}/{name}.mp4', acodec='copy', vcodec='copy')
-        .run()
-    )
+    # adds the video and audio as an input for ffmpeg
+    video = ffmpeg.input(video_path)
+    audio = ffmpeg.input(audio_path)
+
+    # Runs the FFMPEG module to join the audio and video
+    ffmpeg.output(
+        video, audio, f'{final_path}/{name}.mp4', acodec='copy', vcodec='copy').run()
 
 
 def interface():
-    print("Welcome to the Pytube Downloader!")
-    url = input("URL: ")
+    """
+    User interface - Through the command line
+    """
+    url = input("URL: ")  # User inputs the URL of the YouTube Video
 
-    yt = YouTube(url)
+    yt = YouTube(url)  # Initializes YouTube object
 
+    # Uses os module to get path to file
     CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 
+    # creates download folder and sets it as path
     path = os.path.join(CURR_DIR, 'downloads')
 
+    # Video Stream
     yt.streams.get_highest_resolution().download(output_path=f'{path}/video')
+    # Audio Stream
     yt.streams.get_by_itag(139).download(output_path=f'{path}/audio')
 
-    title = []
-    for char in yt.title:
-        if char == "|":
-            title.append("")
-        else:
-            title.append(char)
-    title = "".join(title)
-    print("Downloading... ", title)
-    copy(f'{path}/video/{title}.mp4',
-         f'{path}/audio/{title}.mp4', f'{title}', f'{path}')
+    # Gets the name of the downloaded files using the file in the video directory
+    video_filename = os.listdir(f'{path}/video')[0]
 
+    # Calls the merge function
+    merge(f'{path}/video/{video_filename}',
+          f'{path}/audio/{video_filename}', video_filename[:-4], path)
+
+    # Deletes the video and audio directories
     shutil.rmtree(os.path.join(path, 'audio'))
     shutil.rmtree(os.path.join(path, 'video'))
+
 
 interface()
